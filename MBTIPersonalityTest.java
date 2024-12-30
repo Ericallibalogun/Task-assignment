d28dca57170650bfc65c9e27b7edfca746406073
Ericalli <allieric28@gmail.com>
2024-12-30 21:53:43 +0100

    committed

---
+++ MBTIPersonalityTest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class MBTIPersonalityTest{
+	public static void main(String... ericalli){
+	Scanner userInput = new Scanner(System.in);
+		
+		System.out.println("---------------------------");
+		System.out.println("MBTI Personality Test");
+		System.out.println("---------------------------");
+
+		System.out.println("What is ur name? ");
+		String  nameResponse = userInput.next();
+
+		System.out.println("Choose what best defines you:");
+
+
+}
+}

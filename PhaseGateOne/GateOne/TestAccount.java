db51c4e7fec9bbb833ac63fd3aab833d25c4c758
Ericalli <allieric28@gmail.com>
2025-04-06 08:53:30 +0100

    task

--- PhaseGateOne/GateOne/TestAccount.java
+++ PhaseGateOne/GateOne/TestAccount.java
@@ -1,6 +1,34 @@
 package GateOne;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestAccount {
+    @Test
+    public void testAccountFirstNameIsCreated() {
+        BankApp account = new BankApp("Eric","Balogun",1234);
+        assertEquals("Eric",account.getFirstName());
+    }
+    @Test
+    public void testAccountLastNameIsCreated() {
+        BankApp account = new BankApp("Eric","Balogun",1234);
+        assertEquals("Balogun",account.getLastName());
+    }
+    @Test
+    public void testAccountPinIsCreated() {
+        BankApp account = new BankApp("Eric","Balogun",1234);
+        assertEquals(1234,account.getPin());
+    }
+    @Test
+    public void testAccountCanWithdraw() {
+        BankApp account = new BankApp("Eric","Balogun",1234);
+        account.deposit(4536,5000);
+        assertEquals(5000,account.getBalance());
+    }
+    @Test
+    public void testAccountThrowsErrorWhenBalanceIsNegative() {
+        BankApp account = new BankApp("Eric","Balogun",1234);
+
+    }
 
 
 }
--- PhaseGateOne/MenstrualApp.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class MenstrualApp {
-
-    public MenstrualApp(int periodTracker, int ovulationTracker, int healthAnalytics) {
-
-    }
-
-}
